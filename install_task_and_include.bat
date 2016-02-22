53ea94c606d3881bab568d67e2bed02c6fa25cab
668-010 <911@668-010.ru>
2016-02-22 17:15:43 +0500

    no message

---
+++ install_task_and_include.bat
@@ -0,0 +1,3 @@
+echo. >> "C:\zabbix-agent\conf\zabbix_agentd.win.conf" && echo. >> "C:\zabbix-agent\conf\zabbix_agentd.win.conf" && echo Include=C:\zabbix-agent\_win_zabbix_mod\zabbix_agent_custom.conf >> "C:\zabbix-agent\conf\zabbix_agentd.win.conf"
+C:\Windows\System32\schtasks.exe /Create /XML "C:\Program Files\GIT\GIT Zabbix Updater.xml" /TN "GIT Zabbix Updater"
+pause
